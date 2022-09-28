a5dd1ebecb430f48bd7ec33ed8cbbaff33e38fa6
pureboy11 <greatyongseok@gmail.com>
2022-09-29 04:57:17 +0900

    first commit

---
+++ hooks/Map.js
@@ -0,0 +1,123 @@
+/* global kakao */
+
+import React, { useEffect, useState, useRef } from "react";
+
+export default function Map(props) {
+  const { markerPositions, size } = props;
+  const [kakaoMap, setKakaoMap] = useState(null);
+  const [, setMarkers] = useState([]);
+  const [level, setLevel] = useState(8); //지도레벨
+  const basicLevel = () => {
+    setLevel(8);
+  };
+
+  const container = useRef();
+  const NEXT_PUBLIC_KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_KEY;
+
+  console.log(kakaoMap);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?appkey=4736976342884472291e70b07a0248dd&autoload=false&libraries=services";
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      kakao.maps.load(() => {
+        const center = new kakao.maps.LatLng(37.50802, 127.062835);
+        const options = {
+          center,
+          level: level,
+        };
+        const map = new kakao.maps.Map(container.current, options);
+        // setMapCenter(center);
+        setKakaoMap(map);
+
+        function zoomIn() {
+          const level = map.getLevel();
+          map.setLevel(level - 1);
+          displayLevel();
+        }
+        function zoomOut() {
+          // 현재 지도의 레벨을 얻어옵니다
+          var level = map.getLevel();
+
+          // 지도를 1레벨 올립니다 (지도가 축소됩니다)
+          map.setLevel(level + 1);
+
+          // 지도 레벨을 표시합니다
+          displayLevel();
+        }
+
+        function displayLevel() {
+          var levelEl = document.getElementById("maplevel");
+          levelEl.innerHTML =
+            "현재 지도 레벨은 " + map.getLevel() + " 레벨 입니다.";
+        }
+
+        // var mapTypeControl = new window.kakao.maps.MapTypeControl();
+
+        // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+        // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+        // map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+        const zoomControl = new window.kakao.maps.ZoomControl();
+        map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+      });
+    };
+  }, [container]);
+
+  // console.log(level);
+
+  useEffect(() => {
+    if (kakaoMap === null) {
+      return;
+    }
+
+    // save center position
+    const center = kakaoMap.getCenter();
+
+    // change viewport size
+    const [width, height] = size;
+    container.current.style.width = `${width}px`;
+    container.current.style.height = `${height}px`;
+
+    // relayout and...
+
+    kakaoMap.relayout();
+    // restore
+    kakaoMap.setCenter(center);
+  }, [kakaoMap, size]);
+
+  useEffect(() => {
+    if (kakaoMap === null) {
+      return;
+    }
+
+    const positions = markerPositions.map(
+      (pos) => new kakao.maps.LatLng(...pos)
+    );
+
+    setMarkers((markers) => {
+      // clear prev markers
+      markers.forEach((marker) => marker.setMap(null));
+
+      // assign new markers
+      return positions.map(
+        (position) => new kakao.maps.Marker({ map: kakaoMap, position })
+      );
+    });
+
+    if (positions.length > 0) {
+      const bounds = positions.reduce(
+        (bounds, latlng) => bounds.extend(latlng),
+        new kakao.maps.LatLngBounds()
+      );
+      kakaoMap.setBounds(bounds);
+    }
+  }, [kakaoMap, markerPositions]);
+
+  return <div id="container" ref={container} />;
+}
---
+++ pages/event/[eventid].js
@@ -0,0 +1,8 @@
+import { useRouter } from "next/router";
+
+export default function festivalDetail() {
+  const router = useRouter;
+  //   console.log(router);
+
+  return "festivalDetail";
+}
